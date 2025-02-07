4cbe203b224fce20c896ae95c23e4862e9f7b48a
Alexander Eklöf <alexander.eklof@iths.se>
2025-02-07 14:08:37 +0100

    SQL Controller API

---
+++ SQLControllerTest/Controllers/PotatoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLControllerTest.Data;
+using SQLControllerTest.Models;
+
+namespace SQLControllerTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PotatoController : ControllerBase
+    {
+        private readonly DataContext _Dbcontext;
+
+        public PotatoController(DataContext dbContext)
+        {
+            _Dbcontext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Potato>>> GetAllPotatoes()
+        {
+            var potatoes = await _Dbcontext.Potatoes.ToListAsync();
+            return Ok(potatoes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Potato>> GetPotatoById(int id)
+        {
+            var potato = await _Dbcontext.Potatoes.FindAsync(id);
+            if (potato == null)
+            {
+                return NotFound();
+            }
+            return Ok(potato);
+        }
+    }
+}
---
+++ SQLControllerTest/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLControllerTest.Models;
+
+namespace SQLControllerTest.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Potato> Potatoes { get; set; }
+    }
+}
